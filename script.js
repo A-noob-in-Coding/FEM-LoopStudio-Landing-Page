1496aaf84a47d596b3e535d012b342f318e4401a
A_noob_in_coding <naeemabdulrehman77@gmail.com>
2024-08-14 19:33:25 +0500

    added menu button functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let button = document.getElementById("menu-button")
-let nav = document.getElementById("nav-menu")
+let navmenu = document.getElementById("nav-menu")
+navmenu.classList.toggle("visible")
 button.addEventListener("focus",()=>{
-    console.log("clicked")
-        nav.classList.toggle("visible")
+    button.classList.toggle("transition");
+    console.log(button.classList)
+    navmenu.classList.toggle("visible")
+    
 })
